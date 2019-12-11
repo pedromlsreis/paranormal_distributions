d684261319804edad9a03c82ae774f0e85f03de9
Pedro Reis <pedromlsreis@gmail.com>
2019-12-11 23:37:02 +0000

    flake8 changes

--- project.py
+++ project.py
@@ -7,21 +7,18 @@ Created on Wed Nov 13 07:09:18 2019
 """
 
 # importing all the libraries
-import os
-import numpy as np
 import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
 from pip._internal import main as pipmain
 
 try:
     import pandas_profiling
-except:
+except ImportError as e:
+    print(e.args)
     pipmain(['install', 'pandas_profiling'])
     import pandas_profiling
 
 from utils.data_extraction import data_extract
-from utils.preprocessing import preprocessing_dataframe, adding_dummies, remove_outliers
+from utils.preprocessing import preprocessing_df, add_dummies, remove_outliers
 
 
 # setting display options
@@ -35,28 +32,35 @@ pd.set_option('display.float_format', '{:.2f}'.format)
 
 my_path = r'.\data\insurance.db'
 
-def run(path = str, profile = bool):
+
+def run(path=str, profile=bool):
     # data extraction
     _, df = data_extract(path)
-
-    #exploring the data
+    # exploring the data
     if profile:
-        prof = df.profile_report(style={'full_width':True}, title='Pandas Profiling Report')
+        prof = df.profile_report(
+            style={'full_width': True},
+            title='Pandas Profiling Report'
+            )
         prof.to_file(output_file="./out/df_profiling.html")
 
     # data preprocessing
-    (df, dups_df) = preprocessing_dataframe(df)
+    (df, dups_df) = preprocessing_df(df)
 
     # adding dummy variables
-    df = adding_dummies(df, cols = ['Area', 'Education'])
+    df = add_dummies(df, cols=['Area', 'Education'])
 
     # removing outliers
-    #testing to remove outliers using z score. But some of the results are fucking waaaaaack!
-    (df, outliers_count) = remove_outliers(df, cols=['Motor','Household','Health','Life','Work_Compensation'])
+    # testing to remove outliers using z score.
+    # But some of the results are fucking waaaaaack!
+    (df, outliers_count) = remove_outliers(
+        df,
+        cols=['Motor', 'Household', 'Health', 'Life', 'Work_Compensation']
+        )
 
     print(f"outlier_count:\n{outliers_count}\n")
     print(df.head(2))
 
 
 if __name__ == "__main__":
-    run(path = my_path, profile = False)
+    run(path=my_path, profile=False)
