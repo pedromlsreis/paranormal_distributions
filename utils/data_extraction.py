0129f9e39d8b1c67995613f802149de9b1410f7f
Pedro Reis <pedromlsreis@gmail.com>
2019-12-11 23:51:13 +0000

    flake8 changes

--- utils/data_extraction.py
+++ utils/data_extraction.py
@@ -1,41 +1,44 @@
 import sqlite3
 import pandas as pd
 
+
 def data_extract(path_to_db):
     # connect to the database
     conn = sqlite3.connect(path_to_db)
-    
+
     query = """
         SELECT
-    	e."Customer Identity",
-    	"First Policy´s Year",
-    	"Brithday Year",
-    	"Educational Degree",
-    	"Gross Monthly Salary",
-    	"Geographic Living Area",
-    	"Has Children (Y=1)",
-    	"Customer Monetary Value",
-    	"Claims Rate",
+        e."Customer Identity",
+        "First Policy´s Year",
+        "Brithday Year",
+        "Educational Degree",
+        "Gross Monthly Salary",
+        "Geographic Living Area",
+        "Has Children (Y=1)",
+        "Customer Monetary Value",
+        "Claims Rate",
         l."Premiums in LOB: Motor",
-    	l."Premiums in LOB: Household",
-    	l."Premiums in LOB: Health",
-    	l."Premiums in LOB:  Life",
-    	l."Premiums in LOB: Work Compensations"
-    	FROM
+        l."Premiums in LOB: Household",
+        l."Premiums in LOB: Health",
+        l."Premiums in LOB:  Life",
+        l."Premiums in LOB: Work Compensations"
+        FROM
         Engage AS e
-    	JOIN LOB AS l ON l."Customer Identity" = e."Customer Identity"
+        JOIN LOB AS l ON l."Customer Identity" = e."Customer Identity"
         ORDER BY
-    	e."Customer Identity";
+        e."Customer Identity";
     """
-    
+
     data_df = pd.read_sql_query(query, conn)
     df = data_df.copy()  # let's keep a copy of the original data
-    
-    #remaining column names to manageable variable names
-    column_names = ['ID', 'First_Policy', 'Birthday', 'Education', 'Salary', 'Area', 'Children', 'CMV',
-                    'Claims', 'Motor', 'Household', 'Health', 'Life', 'Work_Compensation']
-    #renaming the columns
+
+    # remaining column names to manageable variable names
+    column_names = ['ID', 'First_Policy', 'Birthday', 'Education',
+                    'Salary', 'Area', 'Children', 'CMV', 'Claims',
+                    'Motor', 'Household', 'Health', 'Life',
+                    'Work_Compensation']
+    # renaming the columns
     df.columns = column_names
-    #seting 'ID' as index
-    df.set_index('ID', inplace = True, drop = True)
+    # seting 'ID' as index
+    df.set_index('ID', inplace=True, drop=True)
     return data_df, df
--- utils/preprocessing.py
+++ utils/preprocessing.py
@@ -1,6 +1,5 @@
-import pandas as pd
 import numpy as np
-from scipy import stats
+import pandas as pd
 
 """
 Steps to follow, according to the lectures:
@@ -22,36 +21,51 @@ Data preprocessing
 
 """
 
-#Cleaning the data
+
+# Cleaning the data
 def cleaning_df(df):
-    df.loc[df["Birthday"] < 1900, "Birthday"] = np.nan  # turning impossible values into NaN
+    # turning impossible values into NaN
+    df.loc[df["Birthday"] < 1900, "Birthday"] = np.nan
     df.loc[df["First_Policy"] > 2020, "First_Policy"] = np.nan
-    df["Education"] = df["Education"].str.extract(r"(\d)").astype(np.float)  # turning Education into numeric
+    # turning Education into numeric
+    df["Education"] = df["Education"].str.extract(r"(\d)").astype(np.float)
     return df
 
 
-#Creating Dummy variables for Area and Education
-def add_dummies(df, cols):
-    """Adds dummy columns to selected variables using the One Hot Encoding method. Drops the first column."""
+# Creating Dummy variables for Area and Education
+def adding_dummies(df, cols):
+    """Adds dummy columns to selected variables using the One Hot Encoding method.
+    Drops the first column."""
     df_with_dummies = pd.get_dummies(df, columns=cols, drop_first=True)
     return df_with_dummies
 
 
-#Dealing with Missing Values
+# Dealing with Missing Values
 
 
+# Dealing with Outliers
 
-#Dealing with Outliers
 
+# another way, but it needs a df with only numeric columns that have outliers
+def outlier_conditions(df):
+    """
+    Sets the condition for the identification of outliers in a dataframe
+    """
+    return ~(np.abs(df - df.mean()) > (3 * df.std()))
 
-#another way, but it needs a dataframe with columns that are numeric and have outliers
 
 def remove_outliers(df, cols):
-    """Removes outliers and replace them by NaNs.
-    Selected columns must be numerical."""
-    outliers_count = ((df[cols] == df[~(np.abs(df - df.mean()) > (3 * df.std()))][cols]) == False)[cols].sum()
+    """
+    Removes outliers and replace them by NaNs.
+    Selected columns must be numerical.
+    """
+    outlier_df_cond = outlier_conditions(df)
+    outliers_count = (
+        (df[cols] == df[outlier_df_cond][cols]) == False
+        )[cols].sum()
     temp_df = df[cols].copy()
-    temp_df = temp_df[~(np.abs(temp_df - temp_df.mean()) > (3 * temp_df.std()))]
+    outlier_tempdf_cond = outlier_conditions(temp_df)
+    temp_df = temp_df[outlier_tempdf_cond]
     df.loc[:, cols] = temp_df.loc[:, cols].copy()
     return df, outliers_count
 
@@ -60,12 +74,12 @@ def remove_outliers(df, cols):
 def standardize_data(df, cols):
     """Standardizes data from `cols`.
     """
-    
     return df
 
 
-#Data transformation
+# Data transformation
 def preprocessing_df(df):
     df = cleaning_df(df)
-    dups_df = df[df.duplicated(keep="first")].copy() # duplicated rows (showing only the duplicates)
+    # duplicated rows (showing only the duplicates)
+    dups_df = df[df.duplicated(keep="first")].copy()
     return df, dups_df
