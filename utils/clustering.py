7bd7235c74c30917527530a76b87e82c7072dc1c
Pedro Reis <pedromlsreis@gmail.com>
2019-12-26 21:15:39 +0000

    Start clustering.

---
+++ utils/clustering.py
@@ -0,0 +1,10 @@
+# Cluster analysis
+
+"""
+1. Define variables to use
+2. Define similarity/dissimilarity criterion between entities
+3. Define a clustering algorithm to create groups of similar entities
+4. Analyse it and validate the resulting solution.
+"""
+
+import numpy as np
